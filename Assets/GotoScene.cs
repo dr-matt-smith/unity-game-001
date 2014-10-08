25e6b41608bc1a0f423e152ca8919c8daf776abc
Matt Smith <dr_matt_smith@me.com>
2014-10-08 15:27:59 +0100

    v.05 - about scene only

---
+++ Assets/GotoScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GotoScene : MonoBehaviour {
+
+	public void GotoMAIN_MENU(){
+		Application.LoadLevel(0);
+	}
+	
+	public void GotoABOUT(){
+		Application.LoadLevel(1);
+	}
+	
+}
